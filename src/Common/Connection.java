eeb69c358475add4f263108f8ea0a4b6bc79e1d9
huy.ho <huyho1210@gmail.com>
2020-07-01 01:20:16 +0700

    update

--- src/Common/Connection.java
+++ src/Common/Connection.java
@@ -3,7 +3,6 @@ package Common;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
