4459f7b95c84883d11e7bb7335a0b4ea13ee48a9
Quoc Huy <huy.ho>
2020-07-01 08:36:53 +0700

    update key

--- src/Common/Connection.java
+++ src/Common/Connection.java
@@ -13,10 +13,10 @@ public class Connection {
 	@Before
 	public void Connection() {
 		System.setProperty("webdriver.chrome.driver", "lib//chromedriver.exe");
-//		ChromeOptions options = new ChromeOptions();
-//		options.addArguments("headless");
-//		driver = new ChromeDriver(options);
-		driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
+		driver = new ChromeDriver(options);
+//		driver = new ChromeDriver();
 		driver.get(CommonLink.URL_LOGIN);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
